83c3a3b8515e04c08a0905a97ecef6c220f3d9cf
msı <ms@YASIN>
2020-11-15 19:22:57 +0300

    Add project files.

---
+++ TurkceEnglishForm/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace TurkceEnglishForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rtbMetin = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.rtbSonuc = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // rtbMetin
+            // 
+            this.rtbMetin.AccessibleRole = System.Windows.Forms.AccessibleRole.ScrollBar;
+            this.rtbMetin.Location = new System.Drawing.Point(12, 64);
+            this.rtbMetin.Name = "rtbMetin";
+            this.rtbMetin.Size = new System.Drawing.Size(776, 142);
+            this.rtbMetin.TabIndex = 0;
+            this.rtbMetin.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(246, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(338, 32);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Çevrilecek Metni Giriniz";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(252, 222);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(302, 33);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Dönüştür";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // rtbSonuc
+            // 
+            this.rtbSonuc.AccessibleRole = System.Windows.Forms.AccessibleRole.ScrollBar;
+            this.rtbSonuc.Location = new System.Drawing.Point(12, 277);
+            this.rtbSonuc.Name = "rtbSonuc";
+            this.rtbSonuc.Size = new System.Drawing.Size(776, 137);
+            this.rtbSonuc.TabIndex = 3;
+            this.rtbSonuc.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.rtbSonuc);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.rtbMetin);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox rtbMetin;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox rtbSonuc;
+    }
+}
+
---
+++ TurkceEnglishForm/Transformation.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TurkceEnglishForm
+{
+    class Transformation
+    {
+        string[] TurkceKarakterler = { "ç", "ğ","ı", "ö", "ş", "ü", "Ç", "Ğ", "Ö","İ","Ü","Ş" };
+        string[] IngilizceKarakterler = { "c", "g", "i", "o", "s", "u", "C", "G", "O", "I", "U", "S" };
+        public string Trans(string metin)
+        {
+            string kelime = "";
+            for (int i = 0; i <= metin.Length- 1; i++)
+            {
+                if (!(TurkceKarakterler.Contains(metin[i].ToString())))
+                {
+                    kelime += metin[i];
+                }
+                else
+                {
+                    for (int j = 0; j <= TurkceKarakterler.Length-1; j++)
+                    {
+                        if (metin[i].ToString() == TurkceKarakterler[j])
+                        {
+                            kelime += IngilizceKarakterler[j];
+                        }
+
+                    }
+
+                }    
+                
+                
+
+
+            }
+            return kelime;
+
+        }
+        
+        
+    }
+}
